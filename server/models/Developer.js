e388d0c779e9ae11bf7f5c2da1c130de1200f1e1
Meaghan Kish <106093711+Mkish1220@users.noreply.github.com>
2022-08-31 20:34:24 -0400

    update and rename to developer.js
    let me know if contributions is set up liked we talk or is it needed 
    contributions: ["contributions": contributions] ?

---
+++ server/models/Developer.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
+
+const developerSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  contributions: {
+    type: String
+});
+
+// set up pre-save middleware to create password
+developerSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+developerSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const Developer = mongoose.model('Developer', developerSchema);
+
+module.exports = Developer;
