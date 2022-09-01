7bb7ca2eaeb162c7530dfe57bf2158722caddeee
Kris Marshall <rflctnofu@hotmail.com>
2022-09-01 09:51:04 -0400

    slight superfluous edit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { FaUserCircle } from 'react-icons/fa'
 const Header = () => {
     return (
         <>
-            <header className="bg-neutral text-neutral-content flex justify-between h-32 items-center ">
+            <header className="bg-gradient-to-br from-neutral to-slate-500 text-neutral-content flex justify-between h-32 items-center ">
                 <div className="pl-8 flex items-center">
                     <FaUserCircle size={70} />
                     <h1 className="pl-2 text-4xl font-bold">SupporDev</h1>
