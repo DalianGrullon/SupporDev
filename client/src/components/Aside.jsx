c5f992b08a4b9cef7e3acc1bed9ce1e94f76ab0b
Kris Marshall <rflctnofu@hotmail.com>
2022-09-01 10:53:47 -0400

    made basic aside card component, small adjustments to aside element width

--- client/src/components/Aside.jsx
+++ client/src/components/Aside.jsx
@@ -1,18 +1,12 @@
-
+import AsideCard from "./AsideCard"
 
 const Aside = () => {
     return (
         <>
-            <aside className="float-left md:w-2/12 h-100 bg-base-200 container">
-                <div className="bg-neutral m-8 h-auto w-auto">
-                    Some Text
-                </div>
-                <div className="bg-neutral m-8">
-                    Some Text
-                </div>
-                <div className="bg-neutral m-8">
-                    Some Text
-                </div>
+            <aside className="float-left w-auto bg-base-100 overflow-auto items-center">
+                <AsideCard />
+                <AsideCard />
+                <AsideCard />
             </aside>
 
         </>
