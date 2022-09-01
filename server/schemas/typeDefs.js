d854a670eb72bb6120dfb090420adb42c0a99e71
Dalian Grullon <dalian.business@aol.com>
2022-09-01 13:23:13 -0400

    Format update

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,17 +2,16 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-
-
 type Developer {
-   _id: ID
-   username: String
-   contribution: [Contribution]
+  _id: ID
+  username: String
+  contribution: [Contribution]
 }
+
 type Contribution {
-    _id: ID
-    title: String
-    description: String
+  _id: ID
+  title: String
+  description: String
 }
 
 type Request {
@@ -34,11 +33,9 @@ type developerAuth {
 }
 
 type requesterAuth {
-token: ID
-requester: Requester
+  token: ID
+  requester: Requester
 } 
-
-
 
 type Query {
   developer(_id: ID!, userName: String!): [Developer]
@@ -47,19 +44,15 @@ type Query {
   contribution: [Contribution]
 }
 
-
-
 type Mutation {
   developerLogin(userName: String!, password: String!): developerAuth
   requesterLogin(userName: String!, password: String!): requesterAuth
-    addDeveloper(userName: String!, firstName: String!, lastName: String!, email: String!,
-password: String!): developerAuth
-    addRequester(userName: String!, firstName: String!, lastName: String!, email: String!,
-password: String!): requesterAuth
-    addContribution(userName: String!,  _id: ID!): Developer
-    addRequest(title: String!, description: String!, _id: ID!): Request
-    updateRequest(_id: ID!, title: String!, description: String!): Request
-    deleteRequest(_id: ID!, title: String!, description: String!): Request
+  addDeveloper(userName: String!, firstName: String!, lastName: String!, email: String!, password: String!): developerAuth
+  addRequester(userName: String!, firstName: String!, lastName: String!, email: String!, password: String!): requesterAuth
+  addContribution(userName: String!,  _id: ID!): Developer
+  addRequest(title: String!, description: String!, _id: ID!): Request
+  updateRequest(_id: ID!, title: String!, description: String!): Request
+  deleteRequest(_id: ID!, title: String!, description: String!): Request
 }
 `;
 
