8223390d12234458c0ffc8c23ce207a98c3d8732
Kris Marshall <rflctnofu@hotmail.com>
2022-08-31 20:34:04 -0400

    basic header and nav done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,23 @@
 import Nav from './Nav'
+import { FaUserCircle } from 'react-icons/fa'
 
 const Header = () => {
     return (
         <>
-            <div>Header</div>
-            <Nav />
+            <header className="bg-neutral text-neutral-content flex justify-between h-32 items-center ">
+                <div className="pl-8 flex items-center">
+                    <FaUserCircle size={70} />
+                    <h1 className="pl-2 text-4xl font-bold">SupporDev</h1>
+                </div>
+
+                <div className="pr-8">
+                    <Nav />
+                </div>
+            </header>
+
+
+
+
         </>
     )
 }
