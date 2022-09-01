92c477cd951f4aa84690badac37891cbb3849b57
Kris Marshall <rflctnofu@hotmail.com>
2022-09-01 17:23:19 -0400

    kindof fixed the scrolling of the aside, styled footer a little, need to redo ProjectCard.jsx....I lost it after working in main - doh

--- client/src/components/Aside.jsx
+++ client/src/components/Aside.jsx
@@ -1,9 +1,10 @@
 import AsideCard from "./AsideCard"
 
 const Aside = () => {
+
     return (
         <div className="">
-            <aside className="float-left w-auto bg-base-100 h-1/2 items-center overflow-y-scroll">
+            <aside className="  bg-base-100 h-2/3 items-center overflow-auto absolute" >
                 <AsideCard />
                 <AsideCard />
                 <AsideCard />
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,27 +1,42 @@
 import { SiGithub } from "react-icons/si";
 
 const Footer = () => {
+    // const scale = {
+    //     scale: 
+    // }
     return (
         <div id="Contact">
-            <div className="FooterContainer bg-neutral text-neutral-content pb-2">
-                <h1 className="text-xl text-center font-serif">Socials</h1>
-                <div className="m-4 text-center flex justify-center space-x-2">
+            <div className="bg-neutral text-neutral-content py-2">
+                {/* <h1 className="text-xl text-center font-serif">Socials</h1> */}
+                <div className="m-4 text-center flex justify-around font-mono ">
+                    <div className="flex items-center">
                     <a href="https://github.com/DalianGrullon" target='_blank' rel="noreferrer">
                         {" "}
-                        <SiGithub size={30} />
+                        <SiGithub className="hover:text-base-200 hover:scale-105 duration-300 " size={60} />
                     </a>
+                    <span className="pl-4 text-xl">Dalian Grullon</span>
+                    </div>
+                    <div className="flex items-center">
                     <a href="https://github.com/Mkish1220" target='_blank' rel="noreferrer">
                         {" "}
-                        <SiGithub size={30} />
+                        <SiGithub className="hover:text-base-200 hover:scale-105 duration-300" size={60} />
                     </a>
-                    <a href="https://github.com/RflctnOfU" target='_blank' rel="noreferrer">
+                    <span className="pl-4 text-xl">Meaghan Kish</span>
+                    </div>
+                    <div className="flex items-center">
+                    <a href="https://github.com/RflctnOfU" target='_blank' rel="noreferrer" >
                         {" "}
-                        <SiGithub size={30} />
+                        <SiGithub className="hover:text-base-200 hover:scale-105 duration-300" size={60} />
                     </a>
+                    <span className="pl-4 text-xl">Kristofer Marshall</span>
+                    </div>
+                    <div className="flex items-center">
                     <a href="https://github.com/Zachary-Maddox" target='_blank' rel="noreferrer">
                         {" "}
-                        <SiGithub size={30} />
+                        <SiGithub className="hover:text-base-200 hover:scale-105 duration-300" size={60} />
                     </a>
+                    <span className="pl-4 text-xl">Zachary Maddox</span>
+                    </div>
                 </div>
             </div>
         </div>
