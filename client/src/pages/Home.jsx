fa800fd3b6ba650ebec1e2ab2c861b0c3a36477f
Zachary-Maddox <z19s96p@aol.com>
2022-09-01 12:20:51 -0500

    added card header for user. Contains Img,Username,and Title

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,24 @@
-
-
 const Home = () => {
     return (
-        <div className="text-4xl text-primary">Home</div>
-    )
-}
+        <div>
+            <div className="Container border-2 border-zinc-800 flex">
+                <div className="basis-1/3 flex justify-start items-center">
+                <img
+                    id="ProfileImage"
+                    className="border-2 rounded-full border-zinc-800 w-12"
+                    src="logo192.png"
+                    alt="Image"
+                />
+                </div>
+                <h2 id="Username" className="basis-1/3 flex justify-center items-center">
+                    Username
+                </h2>
+                <h3 id="Title" className="basis-1/3 flex justify-center items-center">
+                    Title
+                </h3>
+            </div>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
