fcd7c1d6394b50d0c7aca29e51ceffc6c140227b
Kris Marshall <rflctnofu@hotmail.com>
2022-09-02 18:43:01 -0400

    started signup form, made auth, mutations, and queries files in utils

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import {useState} from 'react';
+import Auth from '../utils/auth'
+import {ADD_USER} from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 const Signup = () => {
+    const [userFormData, setUserFormData] = useState({ username: '', firstName: '', lastName: '', email: '', password: '', role: ''})
+
     return (
         <div>Signup</div>
     )
