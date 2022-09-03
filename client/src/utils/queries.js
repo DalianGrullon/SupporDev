670b996769368a42de78564ef1982b29882e2234
Dalian Grullon <dalian.business@aol.com>
2022-09-02 20:11:22 -0400

    Created gql queries to query for requests and requesters respectively

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,33 @@
 import { gql } from "@apollo/client";
+
+// returns a requester with their corresponding requests
+export const QUERY_REQUESTER = gql`
+  query getRequester($id: ID!) {
+    requester(_id: $id) {
+      _id
+      userName
+      email
+      requests {
+        _id
+        title
+        description
+      }
+    }
+  }
+`;
+
+// returns all requests with their corresponding requesters
+export const QUERY_REQUESTS = gql`
+  query getRequests {
+    requests {
+      _id
+      title
+      description
+      requester {
+        _id
+        userName
+        email
+      }
+    }
+  }
+`;
