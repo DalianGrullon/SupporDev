29fd1a355ce82f51b37baa155dd9480a4a72bd64
Kris Marshall <rflctnofu@hotmail.com>
2022-09-03 17:00:39 -0400

    removed flowbite

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("daisyui"),
-    require("@tailwindcss/forms"),
-    require("flowbite/plugin"),
-  ],
+  plugins: [require("daisyui"), require("@tailwindcss/forms")],
   daisyui: {
     themes: ["garden", "dark"],
   },
