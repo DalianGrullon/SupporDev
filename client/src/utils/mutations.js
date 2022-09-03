82edba5cf3c812a4295ddfe3c28e7f801ed6a500
Dalian Grullon <dalian.business@aol.com>
2022-09-02 20:35:29 -0400

    Created mutations for...creating a requester, creating a developer, creating a request, and logging in for both developers and requesters

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,14 +1,55 @@
 import { gql } from "@apollo/client";
 
-export const LOGIN_USER = gql`
-
-
-
+export const ADD_REQUESTER = gql`
+  mutation addRequester($userName: String!, $firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+    addRequester(userName: $userName, firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+      token
+    }
+  }
 `;
 
+export const ADD_REQUEST = gql`
+  mutation addRequest($title: String!, $description: String!, $requester: ID!) {
+    addRequest(title: $title, description: $description, requester: $requester) {
+      _id
+      title
+      description
+      requester {
+        userName
+        email
+      }
+    }
+  }
+`;
 
-export const ADD_USER = gql`
+export const REQUESTER_LOGIN = gql`
+  mutation requesterLogin($email: String!, $password: String!) {
+    requesterLogin(email: $email, password: $password) {
+      token
+      requester {
+        _id
+        userName
+      }
+    }
+  }
+`;
 
+export const ADD_DEVELOPER = gql`
+  mutation addDeveloper($userName: String!, $firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+    addDeveloper(userName: $userName, firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+      token
+    }
+  }
+`;
 
-
+export const DEVELOPER_LOGIN = gql`
+  mutation developerLogin($email: String!, $password: String!) {
+    developerLogin(email: $email, password: $password) {
+      token
+      developer {
+        _id
+        userName
+      }
+    }
+  }
 `;
