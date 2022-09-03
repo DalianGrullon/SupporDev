108fd59201f66931c38003d7649ece9fb1a20aef
Kris Marshall <rflctnofu@hotmail.com>
2022-09-03 12:19:46 -0400

    added flowbite component library

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,26 +1,22 @@
-import Nav from './Nav'
-import { FaUserCircle } from 'react-icons/fa'
+import Nav from "./Nav";
+import { FaUserCircle } from "react-icons/fa";
 
 const Header = () => {
-    return (
-        <>
-        {/* TODO make burger menu for mobile devices */}
-            <header className="bg-gradient-to-br from-neutral-focus to-slate-400 text-neutral-content flex justify-between h-32 items-center ">
-                <div className="pl-8 flex items-center">
-                    <FaUserCircle size={70} />
-                    <h1 className="pl-2 text-4xl font-bold">SupporDev</h1>
-                </div>
+  return (
+    <>
+      {/* TODO make burger menu for mobile devices */}
+      <header className="bg-gradient-to-br from-neutral-focus to-slate-400 text-neutral-content flex justify-between h-32 items-center ">
+        <div className="pl-8 flex items-center">
+          <FaUserCircle size={70} />
+          <h1 className="pl-2 text-4xl font-bold">SupporDev</h1>
+        </div>
 
-                <div className="pr-8">
-                    <Nav />
-                </div>
-            </header>
+        <div className="pr-8">
+          <Nav />
+        </div>
+      </header>
+    </>
+  );
+};
 
-
-
-
-        </>
-    )
-}
-
-export default Header
+export default Header;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require('@tailwindcss/forms')],
+  plugins: [
+    require("daisyui"),
+    require("@tailwindcss/forms"),
+    require("flowbite/plugin"),
+  ],
   daisyui: {
-    themes: ['garden', 'dark'],
+    themes: ["garden", "dark"],
   },
-}
+};
