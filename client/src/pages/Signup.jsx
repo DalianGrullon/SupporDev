3c6e88c3ef2e159c1ad13b193ec8bb1cd0e0cd87
Kris Marshall <rflctnofu@hotmail.com>
2022-09-06 01:02:49 -0400

    logic almost done...token is not recognized on login attempt.  Have to look into this

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -53,20 +53,6 @@ const Signup = () => {
       Auth.login(token);
     }
 
-    // const form = e.currentTarget;
-    // if (form.checkValidity() === false) {
-    //   e.preventDefault();
-    //   e.stopPropagation();
-    // }
-
-    // try {
-    //     const {data} = await addUser({variables: { ...userFormData}});
-
-    // } catch (err) {
-    //     console.error(err);
-    //     setShowAlert(true)
-    // }
-
     setUserFormData({
       userName: "",
       firstName: "",
@@ -78,64 +64,7 @@ const Signup = () => {
   };
 
   return (
-    // <div className="text-primary bg-base-100 col-span-3 col-start-6 row-span-4 row-start-1 mx-auto">
-    //   <form className="rounded-lg" noValidate validated={validated}>
-    // <label htmlFor="type">Choose your type of account</label>
-    // <select className="form-select rounded-lg" name="role" id="role">
-    //   <option value="none" disabled hidden selected>
-    //     Choose account type
-    //   </option>
-    //   <option name="developer" value={userFormData.role}>
-    //     Developer
-    //   </option>
-    //   <option name="requester" value={userFormData.role}>
-    //     Requester
-    //   </option>
-    // </select>
-
-    //     <label htmlFor="username">Username</label>
-    //     <input
-    //       className="form-input rounded-lg"
-    //       type="text"
-    //       name="username"
-    //       placeholder="username"
-    //       onChange={handleInputChange}
-    //     />
-    //     <label htmlFor="firstName">First Name</label>
-    //     <input
-    //       className="form-input rounded-lg"
-    //       type="text"
-    //       name="firstName"
-    //       placeholder="First Name"
-    //       onChange={handleInputChange}
-    //     />
-    //     <label htmlFor="lastName">Last Name</label>
-    //     <input
-    //       className="form-input rounded-lg"
-    //       type="text"
-    //       name="lastName"
-    //       placeholder="Last Name"
-    //       onChange={handleInputChange}
-    //     />
-    //     <label htmlFor="email">Email</label>
-    //     <input
-    //       className="form-input rounded-lg"
-    //       type="text"
-    //       name="email"
-    //       placeholder="email@email.com"
-    //       onChange={handleInputChange}
-    //     />
-    //     <label htmlFor="password">Password</label>
-    //     <input
-    //       className="form-input rounded-lg"
-    //       type="password"
-    //       name="password"
-    //       placeholder="Password"
-    //       onChange={handleInputChange}
-    //     />
-    //   </form>
-    // </div>
-    <div className="block p-6 rounded-lg shadow-lg bg-base-300 max-w-md col-start-6 col-span-4 ">
+    <div className="block p-6 rounded-lg shadow-lg bg-base-300 max-w-md col-start-6 col-span-4 mb-24">
       <form className="form" onSubmit={handleFormSubmit}>
         <select
           className="form-select bg-base-100 rounded-lg mb-6 w-full"
