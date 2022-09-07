7e330796df142af6ac368cfd571a3700de6cf0f1
Meaghan Kish <106093711+Mkish1220@users.noreply.github.com>
2022-09-07 16:56:16 -0400

    update Header.js
    removed comment area

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,15 +3,8 @@ import { FaUserCircle } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <>
-        <div className="pl-8 flex items-center">
-          <FaUserCircle size={70} />
-          <h1 className="pl-2 text-4xl font-bold">SupporDev</h1>
-        </div>
-
-        <div className="pr-8"> */}
+ <>
       <Nav />
-      </header> */}
     </>
   );
 };
