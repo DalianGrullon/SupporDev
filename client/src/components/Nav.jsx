92a28aa58e31582eb69569e9d0ccdd9b8c3fe4cf
Dalian Grullon <dalian.business@aol.com>
2022-09-07 18:23:31 -0400

    Removed images folder

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -31,7 +31,7 @@ const Nav = ({ fixed }) => {
       <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
           <div className="flex">
-            <img src="/images/SupporDev.png" alt="icon" className="h-20" />
+            <img src="/SupporDev.png" alt="icon" className="h-20" />
             <Link
               className="text-4xl font-bold leading-relaxed inline-block mr-4 pl-4 py-2 whitespace-nowrap text-primary-content hover:scale-105 hover:text-blue-400 duration-300 ease-in-out"
               style={{ textShadow: "2px 4px 4px #496551" }}
